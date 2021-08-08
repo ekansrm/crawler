c05b0cbe23ff0a71ac914843b7d42f6a6800ae22
ekansrm <ekansrm0001@163.com>
2021-08-08 13:20:39 +0800

    + refactor

---
+++ app/finance/format_fund_info_1.py
@@ -0,0 +1,321 @@
+import re
+import os
+import json
+import traceback
+from tqdm import tqdm
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import Float
+
+from app.finance.database import Base
+from app.finance.database import DBSession
+from app.finance.database import init_db
+from app.finance.database import read_info_json_from_database
+
+
+def to_float(str_num):
+    if str_num == '--':
+        return None
+    clean = re.sub('[^\-.0-9]', '', str_num)
+    return float(clean)
+
+
+    # noinspection NonAsciiCharacters
+class FundInfo1(Base):
+    __tablename__ = 'fund_info_1'
+    代码 = Column('代码', String(128), primary_key=True)
+    名称 = Column('名称', String(128), primary_key=True)
+    类型 = Column('类型', String(128))
+    规模 = Column('规模', String(128))
+    风格_类型 = Column('风格_类型', String(128))
+    阶段涨幅_今年以来 = Column('阶段涨幅_今年以来', String(128))
+    阶段涨幅_近1周 = Column('阶段涨幅_近1周', String(128))
+    阶段涨幅_近1月 = Column('阶段涨幅_近1月', String(128))
+    阶段涨幅_近3月 = Column('阶段涨幅_近3月', String(128))
+    阶段涨幅_近6月 = Column('阶段涨幅_近6月', String(128))
+    阶段涨幅_近1年 = Column('阶段涨幅_近1年', String(128))
+    阶段涨幅_近2年 = Column('阶段涨幅_近2年', String(128))
+    阶段涨幅_近3年 = Column('阶段涨幅_近3年', String(128))
+    经理_个数 = Column('经理_个数', String(128))
+    经理_1_名字 = Column('经理_1_名字', String(128))
+    经理_1_管理数量 = Column('经理_1_管理数量', String(128))
+    经理_1_从业时间 = Column('经理_1_从业时间', String(128))
+    经理_1_从业年均回报 = Column('经理_1_从业年均回报', String(128))
+    经理_1_从业最大盈利 = Column('经理_1_从业最大盈利', String(128))
+    经理_1_从业最大跌幅 = Column('经理_1_从业最大跌幅', String(128))
+    经理_2_名字 = Column('经理_2_名字', String(128))
+    经理_2_管理数量 = Column('经理_2_管理数量', String(128))
+    经理_2_从业时间 = Column('经理_2_从业时间', String(128))
+    经理_2_从业年均回报 = Column('经理_2_从业年均回报', String(128))
+    经理_2_从业最大盈利 = Column('经理_2_从业最大盈利', String(128))
+    经理_2_从业最大跌幅 = Column('经理_2_从业最大跌幅', String(128))
+    经理_3_名字 = Column('经理_3_名字', String(128))
+    经理_3_管理数量 = Column('经理_3_管理数量', String(128))
+    经理_3_从业时间 = Column('经理_3_从业时间', String(128))
+    经理_3_从业年均回报 = Column('经理_3_从业年均回报', String(128))
+    经理_3_从业最大盈利 = Column('经理_3_从业最大盈利', String(128))
+    经理_3_从业最大跌幅 = Column('经理_3_从业最大跌幅', String(128))
+    风险_年化夏普比率_1年 = Column('风险_年化夏普比率_1年', String(128))
+    风险_年化夏普比率_2年 = Column('风险_年化夏普比率_2年', String(128))
+    风险_年化夏普比率_3年 = Column('风险_年化夏普比率_3年', String(128))
+
+
+def print_header():
+    return ','.join([
+        '名称', '代码', '类型', '规模',
+        '风格-类型',
+        '阶段涨幅-今年以来', '阶段涨幅-近1周', '阶段涨幅-近1月', '阶段涨幅-近3月', '阶段涨幅-近6月', '阶段涨幅-近1年', '阶段涨幅-近2年', '阶段涨幅-近3年',
+        '经理-个数',
+        '经理-1-名字', '经理-1-管理数量', '经理-1-从业时间', '经理-1-从业年均回报', '经理-1-从业最大盈利', '经理-1-从业最大跌幅',
+        '经理-2-名字', '经理-2-管理数量', '经理-2-从业时间', '经理-2-从业年均回报', '经理-2-从业最大盈利', '经理-2-从业最大跌幅',
+        '经理-3-名字', '经理-3-管理数量', '经理-3-从业时间', '经理-3-从业年均回报', '经理-3-从业最大盈利', '经理-3-从业最大跌幅',
+        '风险-年化夏普比率-1年', '风险-年化夏普比率-2年', '风险-年化夏普比率-3年'
+    ])
+
+
+def header():
+    return [
+        '名称', '代码', '类型', '规模',
+        '风格_类型',
+        '阶段涨幅_今年以来', '阶段涨幅_近1周', '阶段涨幅_近1月', '阶段涨幅_近3月', '阶段涨幅_近6月', '阶段涨幅_近1年', '阶段涨幅_近2年', '阶段涨幅_近3年',
+        '经理_个数',
+        '经理_1_名字', '经理_1_管理数量', '经理_1_从业时间', '经理_1_从业年均回报', '经理_1_从业最大盈利', '经理_1_从业最大跌幅',
+        '经理_2_名字', '经理_2_管理数量', '经理_2_从业时间', '经理_2_从业年均回报', '经理_2_从业最大盈利', '经理_2_从业最大跌幅',
+        '经理_3_名字', '经理_3_管理数量', '经理_3_从业时间', '经理_3_从业年均回报', '经理_3_从业最大盈利', '经理_3_从业最大跌幅',
+        '风险_年化夏普比率_1年', '风险_年化夏普比率_2年', '风险_年化夏普比率_3年'
+    ]
+
+
+def format_row(data):
+    rise_data = data['涨幅']['阶段涨幅']
+    rise_data_irow = rise_data['irow']
+    rise_data_icol = rise_data['icol']
+
+    def get_rise_data(row, col):
+        return rise_data['data'][rise_data_irow[row]][rise_data_icol[col]]
+
+    risk_data = data['风险']
+    risk_data_irow = risk_data['irow']
+    risk_data_icol = risk_data['icol']
+
+    def get_risk_data(row, col):
+        return risk_data['data'][risk_data_irow[row]][risk_data_icol[col]]
+
+    return [
+        data['名称'], data['代码'], data['类型'], data['规模'],
+
+        data['风格']['类型'],
+
+        get_rise_data('区间回报', '今年以来'), get_rise_data('区间回报', '近1周'), get_rise_data('区间回报', '近1月'),
+        get_rise_data('区间回报', '近3月'), get_rise_data('区间回报', '近6月'), get_rise_data('区间回报', '近1年'),
+        get_rise_data('区间回报', '近2年'), get_rise_data('区间回报', '近3年'),
+
+        str(len(data['经理'])),
+        data['经理'][0]['名字'], data['经理'][0]['管理数量'], data['经理'][0]['从业时间'],
+        data['经理'][0]['从业年均回报'], data['经理'][0]['从业最大盈利'], data['经理'][0]['从业最大跌幅'],
+        data['经理'][1]['名字'] if len(data['经理']) >= 2 else '--',
+        data['经理'][1]['管理数量'] if len(data['经理']) >= 2 else '--',
+        data['经理'][1]['从业时间'] if len(data['经理']) >= 2 else '--',
+        data['经理'][1]['从业年均回报'] if len(data['经理']) >= 2 else '--',
+        data['经理'][1]['从业最大盈利'] if len(data['经理']) >= 2 else '--',
+        data['经理'][1]['从业最大跌幅'] if len(data['经理']) >= 2 else '--',
+        data['经理'][2]['名字'] if len(data['经理']) >= 3 else '--',
+        data['经理'][2]['管理数量'] if len(data['经理']) >= 3 else '--',
+        data['经理'][2]['从业时间'] if len(data['经理']) >= 3 else '--',
+        data['经理'][2]['从业年均回报'] if len(data['经理']) >= 3 else '--',
+        data['经理'][2]['从业最大盈利'] if len(data['经理']) >= 3 else '--',
+        data['经理'][2]['从业最大跌幅'] if len(data['经理']) >= 3 else '--',
+
+        get_risk_data('年化夏普比率', '1年'), get_risk_data('年化夏普比率', '2年'), get_risk_data('年化夏普比率', '3年')
+
+    ]
+
+
+def write_info_to_database(session: DBSession, rows):
+    rows = [
+        format_row(info_json) for info_json in rows
+    ]
+    for row in rows:
+        rowDict = zip(header(), row)
+        rowDict = {a[0]: a[1] for a in rowDict}
+        session.merge(FundInfo1(**rowDict))
+    session.commit()
+
+
+def print_fund_csv(fund_detail_path, fund_csv_path):
+    fund_detail_list = []
+    for path in os.listdir(fund_detail_path):
+        fund_detail_list.append(os.path.join(fund_detail_path, path))
+
+    fund_csv = [print_header()]
+    for path in fund_detail_list:
+        with open(path, 'r', encoding='utf-8') as fp:
+            fund_detail = json.load(fp)
+            fund_csv.append(','.join(format_row(fund_detail)))
+
+    with open(fund_csv_path, 'w', encoding='gbk') as fp:
+        content = '\r'.join(fund_csv)
+        fp.write(content)
+
+
+# noinspection PyPep8Naming
+class 基金行业(Base):
+    __tablename__ = '基金行业'
+    行业 = Column('行业', String(128), primary_key=True)
+    代码 = Column('代码', String(128), primary_key=True)
+    名称 = Column('名称', String(128))
+
+
+def 更新基金行业(数据库会话: DBSession, 基金信息列表: list):
+    for 基金信息 in tqdm(基金信息列表):
+        try:
+            行公共数据 = {
+                '代码': 基金信息['代码'], '名称': 基金信息['名称']
+            }
+            for 配置 in 基金信息['配置']['配置']:
+                行数据 = dict(行公共数据)
+                行数据['行业'] = 配置
+                数据库会话.merge(基金行业(**行数据))
+            数据库会话.commit()
+        except:
+            pass
+
+
+# noinspection PyPep8Naming
+class 基金经理(Base):
+    __tablename__ = '基金经理'
+    基金代码 = Column('基金代码', String(128), primary_key=True)
+    基金名称 = Column('基金名称', String(128))
+    经理顺位 = Column('经理顺位', Integer())
+    经理名字 = Column('经理名字', String(128), primary_key=True)
+    管理数量 = Column('管理数量', Integer())
+    从业时间 = Column('从业时间', String(128))
+    从业年均回报 = Column('从业年均回报', Float())
+    从业最大盈利 = Column('从业最大盈利', Float())
+    从业最大跌幅 = Column('从业最大跌幅', Float())
+
+
+def 更新基金经理(数据库会话: DBSession, 基金信息列表: list):
+    for 基金信息 in tqdm(基金信息列表):
+        try:
+            行公共数据 = {
+                '基金代码': 基金信息['代码'], '基金名称': 基金信息['名称']
+            }
+            for 序号, 经理 in enumerate(基金信息['经理']):
+                行数据 = dict(行公共数据)
+                行数据['经理顺位'] = 序号 + 1
+                行数据['经理名字'] = 经理['名字']
+                行数据['管理数量'] = int(re.sub('[^0-9]', '', 经理['管理数量']))
+                行数据['从业时间'] = 经理['从业时间']
+                行数据['从业年均回报'] = to_float(经理['从业年均回报'])
+                行数据['从业最大盈利'] = to_float(经理['从业最大盈利'])
+                行数据['从业最大跌幅'] = to_float(经理['从业最大跌幅'])
+                数据库会话.merge(基金经理(**行数据))
+            数据库会话.commit()
+        except :
+            traceback.print_exc()
+
+
+class 基金交易风格(Base):
+    __tablename__ = '基金交易风格'
+    类型 = Column('类型', String(128), primary_key=True)
+    描述 = Column('描述', String(128), primary_key=True)
+    建议 = Column('建议', String(128))
+
+
+def 更新基金交易风格(数据库会话: DBSession, 基金信息列表: list):
+    for 基金信息 in tqdm(基金信息列表):
+        try:
+            数据库会话.merge(基金交易风格(**基金信息['风格']))
+            数据库会话.commit()
+        except :
+            traceback.print_exc()
+
+
+# noinspection NonAsciiCharacters
+class 基金基础信息(Base):
+    __tablename__ = '基金基础信息'
+    代码 = Column('代码', String(128), primary_key=True)
+    名称 = Column('名称', String(128), primary_key=True)
+    类型 = Column('类型', String(128))
+    成立 = Column('成立', Integer())
+    规模 = Column('规模', Float())
+    风格 = Column('风格', String(128))
+    经理 = Column('经理', Integer())
+    配置_集中度 = Column('配置_集中度', Float())
+    阶段涨幅_近1月 = Column('阶段涨幅_近1月', Float())
+    阶段涨幅_近3月 = Column('阶段涨幅_近3月', Float())
+    阶段涨幅_近6月 = Column('阶段涨幅_近6月', Float())
+    阶段涨幅_近1年 = Column('阶段涨幅_近1年', Float())
+    阶段涨幅_近2年 = Column('阶段涨幅_近2年', Float())
+    阶段涨幅_近3年 = Column('阶段涨幅_近3年', Float())
+    风险_年化夏普比率_1年 = Column('风险_年化夏普比率_1年', Float())
+    风险_年化夏普比率_2年 = Column('风险_年化夏普比率_2年', Float())
+    风险_年化夏普比率_3年 = Column('风险_年化夏普比率_3年', Float())
+
+
+def 更新基金基础信息(数据库会话: DBSession, 基金信息列表: list):
+
+    def 构造基金基础信息(data):
+
+        rise_data = data['涨幅']['阶段涨幅']
+        rise_data_irow = rise_data['irow']
+        rise_data_icol = rise_data['icol']
+
+        def get_rise_data(row, col):
+            return rise_data['data'][rise_data_irow[row]][rise_data_icol[col]]
+
+        risk_data = data['风险']
+        risk_data_irow = risk_data['irow']
+        risk_data_icol = risk_data['icol']
+
+        def get_risk_data(row, col):
+            return risk_data['data'][risk_data_irow[row]][risk_data_icol[col]]
+
+        return {
+            '名称': data['名称'],
+            '代码': data['代码'],
+            '类型': data['类型'],
+            '成立': int(re.sub('[^0-9]', '', data['成立'])) if '成立' in data else -1,
+            '规模': to_float(data['规模']),
+            '风格': data['风格']['类型'],
+
+            '经理':  len(data['经理']),
+
+            '配置_集中度': to_float(data['配置']['集中度']),
+
+            '阶段涨幅_近1月':  to_float(get_rise_data('区间回报', '近1月')),
+            '阶段涨幅_近3月':  to_float(get_rise_data('区间回报', '近3月')),
+            '阶段涨幅_近6月':  to_float(get_rise_data('区间回报', '近6月')),
+            '阶段涨幅_近1年':  to_float(get_rise_data('区间回报', '近1年')),
+            '阶段涨幅_近2年':  to_float(get_rise_data('区间回报', '近2年')),
+            '阶段涨幅_近3年':  to_float(get_rise_data('区间回报', '近3年')),
+
+            '风险_年化夏普比率_1年':  to_float(get_risk_data('年化夏普比率', '1年')),
+            '风险_年化夏普比率_2年':  to_float(get_risk_data('年化夏普比率', '2年')),
+            '风险_年化夏普比率_3年':  to_float(get_risk_data('年化夏普比率', '3年')),
+        }
+
+    for 基金信息 in tqdm(基金信息列表):
+        try:
+            行数据 = 构造基金基础信息(基金信息)
+            行数据 = 构造基金基础信息(基金信息)
+            数据库会话.merge(基金基础信息(**行数据))
+            数据库会话.commit()
+        except :
+            traceback.print_exc()
+
+
+if __name__ == '__main__':
+    session = DBSession()
+    init_db()
+    info_jsons = read_info_json_from_database(session)
+    # 更新基金行业(session, info_jsons)
+    # 更新基金经理(session, info_jsons)
+    # 更新基金交易风格(session, info_jsons)
+    更新基金基础信息(session, info_jsons)
+    # abc = to_float('-123.abc.45亿%')
+    # print(abc)
+
+    # session.close()
---
+++ app/finance/howbuy.py
@@ -0,0 +1,398 @@
+import time
+import json
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+import re
+import requests
+from pyquery import PyQuery as pq
+import os
+import traceback
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+browser = webdriver.Chrome(options=chrome_options, executable_path='chromedriver.exe')
+wait = WebDriverWait(browser, 10)
+
+
+def get_table_body_data(table_body_dom):
+    """
+    读取 Table Body DOM 的表格数据
+    :param table_body_dom: 
+    :return: 
+    """
+    data = []
+    for tr in table_body_dom.children().items():
+        row = []
+        for td in tr.children().items():
+            td = td.clone()
+            td.find('span[class*="tips"]').remove()
+            row.append(td.text())
+        data.append(row)
+
+    irow = {}
+
+    for i, a in enumerate(data):
+        irow[a[0]] = i
+
+    icol = {}
+
+    if len(data) > 0:
+        for i, a in enumerate(data[0]):
+            icol[a] = i
+
+    return {
+        'irow': irow,
+        'icol': icol,
+        'data': data
+    }
+
+
+def get_fund_info_url(code):
+    url = 'https://www.howbuy.com/fund/{0}/'.format(code)
+    return url
+
+
+def get_fund_dom_by_pyquery(code):
+    url = get_fund_info_url(code)
+    doc = pq(url)
+    return doc
+
+
+def get_fund_dom_by_browser(code):
+    url = get_fund_info_url(code)
+    browser.get(url)
+    html = browser.page_source
+    doc = pq(html)
+    return doc
+
+
+def get_fund_rise(dom):
+    """
+    获取基金涨幅数据
+    :param dom:
+    :return:
+    """
+    rise_doc = dom(
+        '''div[class="file_jjzf"]'''
+    )
+    rise_type_doc = rise_doc('''div[class="title"]''')
+    rise_type = {}
+    for a_rise_type_doc in rise_type_doc('li').items():
+        rise_type[str(a_rise_type_doc.attr('id'))[-1]] = a_rise_type_doc.text()
+    rise_data_doc = rise_doc('''div[class~="jjzf_content"]''')
+    rise = {}
+    for div in rise_data_doc.items():
+        table_id = str(div.attr('id'))[-1]
+        table_doc = div.children()
+        rise[rise_type[table_id]] = get_table_body_data(table_doc('tbody'))
+    return rise
+
+
+def get_fund_risk(dom):
+    """
+    获取基金风险
+    :param dom:
+    :return:
+    """
+    risk_doc = dom(
+        '''h3:contains("基金风险")'''
+    ).parent().parent()
+
+    risk_table_doc = risk_doc('tbody')
+
+    risk_data = get_table_body_data(risk_table_doc)
+
+    return risk_data
+
+
+def get_fund_config(dom):
+    config_dom = dom(
+        '''h3:contains("行业配置")'''
+    ).parent().parent()
+
+    config_concentration = config_dom(
+        '''b:contains("行业集中度")'''
+    ).children().text()
+
+    config_industry = [a.text() for a in config_dom('''g[class="highcharts-legend-item"]''').items()]
+
+    return {
+        '集中度': config_concentration,
+        '配置': config_industry
+    }
+
+
+def get_fund_manager(dom):
+    """
+    获取基金经理
+    :param dom:
+    :return:
+    """
+
+    def get_a_fun_manager(manager_doc):
+        manager_name = manager_doc(
+            '''span:contains("综合评分")'''
+        ).parent().parent().children().eq(0).text()
+
+        manager_admin_num = manager_doc(
+            '''li:contains('当前管理基金')'''
+        ).children().text()
+
+        manager_admin_time = manager_doc(
+            '''span:contains('从业时间')'''
+        ).text().split('：')[-1]
+
+        manager_roi = manager_doc(
+            '''p:contains('从业年均回报')'''
+        ).parent().children('''p[class="b2"]''').text()
+
+        manager_max_gain = manager_doc(
+            '''p:contains('最大盈利')'''
+        ).parent().children('''p[class="b2"]''').text()
+
+        manager_max_drop = manager_doc(
+            '''p:contains('最大回撤')'''
+        ).parent().children('''p[class="b2"]''').text()
+
+        return {
+            '名字': manager_name,
+            '管理数量': manager_admin_num,
+            '从业时间': manager_admin_time,
+            '从业年均回报': manager_roi,
+            '从业最大盈利': manager_max_gain,
+            '从业最大跌幅': manager_max_drop,
+        }
+
+    managers_doc = dom(
+        '''div[class~="manager_box"]'''
+    )
+
+    managers_info = []
+    for a_manager_doc in managers_doc.items():
+        managers_info.append(get_a_fun_manager(a_manager_doc))
+
+    return managers_info
+
+
+def get_fund_trend(code, date_begin='', date_end='', proxies=None):
+    page = 1
+    html = get_fund_trend_html(code, page, date_begin, date_end, proxies)
+    total_page, data = get_fund_trend_data(code, html)
+
+    while page < total_page:
+        page = page + 1
+        next_html = get_fund_trend_html(code, page, date_begin, date_end, proxies)
+        total_page, more_data = get_fund_trend_data(code, next_html)
+        data.extend(more_data)
+
+    return data
+
+
+def get_fund_trend_url(code, page=1, date_begin='', date_end=''):
+    line_per_page = 20
+    url = 'http://fund.eastmoney.com/f10/F10DataApi.aspx'
+    params = {'type': 'lsjz', 'code': code, 'page': page, 'per': line_per_page, 'sdate': date_begin, 'edate': date_end}
+    param = '&'.join([i[0] + '=' + str(i[1]) for i in params.items()])
+    full_url = url + '?' + param
+    return full_url
+
+
+def get_fund_trend_html(code, page=1, date_begin='', date_end='', proxies=None):
+    """
+    获取 URL 内容
+    :param date_end:
+    :param date_begin:
+    :param line_per_page:
+    :param code:
+    :param page:
+    :param proxies:
+    :return:
+    """
+    full_url = get_fund_trend_url(code, page, date_begin, date_end)
+    rsp = requests.get(full_url, proxies=proxies)
+    rsp.raise_for_status()
+    return rsp.text
+
+
+def get_fund_trend_data(code, html):
+    """
+    获取基金走势数据
+    :param html:
+    :return:
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # 获取总页数
+    pattern = re.compile(r'pages:(.*),')
+    result = re.search(pattern, html).group(1)
+    total_page = int(result)
+
+    # 获取表头
+    head = []
+    for oneHead in soup.findAll("th"):
+        head.append(oneHead.contents[0])
+
+    # 数据存取列表
+    data = []
+
+    # 获取数据
+    for row in soup.findAll("tbody")[0].findAll("tr"):
+        row_records = []
+
+        try:
+            for record in row.findAll('td'):
+                val = record.contents
+
+                # 处理空值
+                if not val:
+                    row_records.append('--')
+                else:
+                    row_records.append(val[0])
+
+            row_records = zip(head, row_records)
+
+            row_records = {
+                i[0]: i[1] for i in row_records
+            }
+
+            row_records['code'] = code
+            row_records['date'] = re.sub('[^0-9]', '', row_records['净值日期'])
+            if not row_records['date'].isdigit():
+                continue
+            row_records['date'] = int(row_records['date'])
+
+            # row_records['单位净值'] = float(row_records['单位净值']) if row_records['单位净值'] != '--' else 0.0
+            # row_records['累计净值'] = float(row_records['累计净值']) if row_records['累计净值'] != '--' else 0.0
+            # row_records['日增长率'] = float(row_records['日增长率'].replace('%', '')) if row_records['日增长率'] != '--' else 0.0
+
+            # 记录数据
+            data.append(row_records)
+        except:
+            pass
+
+
+    # 合并结果
+
+    return total_page, data
+
+
+def get_fund_info(dom):
+    try:
+
+        name = dom(
+            """body > div.main > div.file_top_box > div > div.file_t_righ.rt > div > 
+            div.gmfund_title > div > div.lt > h1""") \
+            .clone().children().remove().end().text()
+
+        code = dom(
+            """body > div.main > div.file_top_box > div > div.file_t_righ.rt > div > 
+            div.gmfund_title > div > div.lt > h1""") \
+            .children().text()
+        code = code.replace('(', '').replace(')', '')
+
+        label = dom(
+            '''p[class="risk"]'''
+        ).text()
+
+        scale = dom(
+            '''li:contains('最新规模')'''
+        ).children().text()
+
+        establish = dom(
+            '''li:contains('成立时间')'''
+        ).children().text()
+
+        risk_info = get_fund_risk(dom)
+
+        manager_info = get_fund_manager(dom)
+
+        investment_style_doc = dom(
+            '''div:contains('当前投资风格')'''
+        ).parent().children('''div[class="right_content"]''')
+
+        investment_style_type = investment_style_doc.children(
+            '''p[class="right_p1"]'''
+        ).text()
+
+        investment_style_desc = investment_style_doc.children(
+            '''div[class="right_text"]'''
+        ).text()
+
+        investment_style_suggest = investment_style_doc.children(
+            '''div[class="suggest_text"]'''
+        ).text()
+
+        rise_info = get_fund_rise(dom)
+
+        config_info = get_fund_config(dom)
+
+        return {
+            '名称': name,
+            '代码': code,
+            '类型': label,
+            '规模': scale,
+            '成立': establish,
+            '风险': risk_info,
+            '经理': manager_info,
+            '风格': {
+                '类型': investment_style_type,
+                '描述': investment_style_desc,
+                '建议': investment_style_suggest,
+            },
+            '涨幅': rise_info,
+            '配置': config_info,
+            '_success': True
+        }
+
+    except Exception as e:
+        traceback.print_exc(e)
+        return {
+            '_success': False
+        }
+
+
+def get_fund_all(code):
+    dom = get_fund_dom_by_browser(code)
+    return get_fund_info(dom)
+
+
+def get_fund_list():
+    r = requests.get('http://fund.eastmoney.com/js/fundcode_search.js')
+    content = r.text
+    content = content[content.find('['):content.find(';')]
+    fund_list = json.loads(content)
+    fund_list = [[a[0], a[2]] for a in fund_list]
+    return fund_list
+
+
+def save_fund_list(fund_list_file, fund_list):
+    with open(fund_list_file, 'w', encoding='utf-8') as fp:
+        fund_list_content = '\r'.join(['{0},{1}'.format(a[0], a[1]) for a in fund_list])
+        fp.write(fund_list_content)
+
+
+def load_fund_list(fund_list_file):
+    fund_code_list = set()
+    with open(fund_list_file, 'r', encoding='utf-8') as fp:
+        for line in fp.readlines():
+            line = line.strip()
+            if len(line) != 0:
+                fund_code_list.add(line.split(','))
+
+
+def get_fund_all_from_list(fund_code_list):
+    for fund_code in tqdm(fund_code_list):
+        fund_detail = get_fund_all(fund_code)
+        with open(os.path.join('data', 'fund_info', 'fund_' + fund_code + '.json'), 'w', encoding='utf-8') as fp:
+            json.dump(fund_detail, fp, indent=2, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    # get_fund_list(os.path.join('data', 'fund_list.txt'))
+    fund_detail = get_fund_all('000434')
+    print(json.dumps(fund_detail, indent=2, ensure_ascii=False))
+    # get_fund_all_from_list(os.path.join('data', 'fund_list.txt'))
+    # print(get_fund_trend('000126', date_begin='1990-01-01', date_end='2099-12-31'))
+#
