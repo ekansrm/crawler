78d70d25c8bac86086ed2eb2dd719cab0ed5361a
ekansrm <ekansrm0001@163.com>
2021-09-21 16:44:10 +0800

    + manhuadb

--- app/manhuadb/book_info.py
+++ app/manhuadb/book_info.py
@@ -1,7 +1,6 @@
 import time
-import json
+import traceback
 from tqdm import tqdm
-import requests
 from pyquery import PyQuery as pq
 import os
 from utils.fs import FsRowDict, build_fill_zero
@@ -18,47 +17,20 @@ class Crawler(object):
         return 'https://www.manhuadb.com'
 
     @staticmethod
-    def manhua_url(bid):
+    def book_url(bid):
         return Crawler.base_url() + '/manhua/' + bid
-
-    @staticmethod
-    def _get_img_url(doc):
-        img_url = doc("""img[class="img-fluid show-pic"]""").attr('src')
-        return img_url
-
-    @staticmethod
-    def get_book_details(book_url, interval=0.1):
-        url = Crawler.base_url() + book_url
-        url_format = url.replace('.html', '') + '_p{0}.html'
-        doc = pq(url=url)
-
-        page_select_doc = doc('div[data-page]')
-        total_page_num = int(page_select_doc.attr('data-total'))
-
-        img_url_list = []
-        img_url_list.append(Crawler._get_img_url(doc))
-
-        for page_num in range(2, total_page_num+1,1):
-            n_url = url_format.format(page_num)
-            n_doc = pq(url=n_url)
-            n_img_url = Crawler._get_img_url(n_doc)
-            img_url_list.append(n_img_url)
-            time.sleep(interval)
-
-        return img_url_list
 
     @staticmethod
     def search_book_brief(keyword):
         url = Crawler.base_url() + '/search?q=' + keyword
         doc = pq(url)
         book_list_doc = doc("""a[class="d-block"]""")
-        book_list = [(e.attr('href').split('/')[-1], e.attr('title'), ) for e in book_list_doc.items()]
+        book_list = [(e.attr('href').split('/')[-1], e.attr('title'),) for e in book_list_doc.items()]
         return book_list
 
-
     @staticmethod
-    def crawl_book_details(bid):
-        url = Crawler.manhua_url(bid)
+    def crawl_book_columns(bid):
+        url = Crawler.book_url(bid)
         doc = pq(url)
 
         book_info_title_doc = doc("""h3[class~="comic_version_title"]""")
@@ -92,6 +64,31 @@ class Crawler(object):
 
         return book_info
 
+    @staticmethod
+    def crawl_book_column_pages(book_url, interval=0.1):
+
+        def get_img_url(page_doc):
+            img_url = page_doc("""img[class="img-fluid show-pic"]""").attr('src')
+            return img_url
+
+        url = Crawler.base_url() + book_url
+        url_format = url.replace('.html', '') + '_p{0}.html'
+        doc = pq(url=url)
+
+        page_select_doc = doc('div[data-page]')
+        total_page_num = int(page_select_doc.attr('data-total'))
+
+        img_url_list = [get_img_url(doc)]
+
+        for page_num in range(2, total_page_num + 1, 1):
+            n_url = url_format.format(page_num)
+            n_doc = pq(url=n_url)
+            n_img_url = get_img_url(n_doc)
+            img_url_list.append(n_img_url)
+            time.sleep(interval)
+
+        return img_url_list
+
     def __init__(self, base_dir):
         self._base_dir = base_dir
         self._row_dict = FsRowDict(os.path.join(base_dir, 'all.json'))
@@ -105,7 +102,7 @@ class Crawler(object):
         u = 1
         for book_id, book_title in book_process_bar:
             book_process_bar.set_description('获取书籍详情: {0}(id={1})'.format(book_title, book_id))
-            book_detail = Crawler.crawl_book_details(book_id)
+            book_detail = Crawler.crawl_book_columns(book_id)
 
             # 将书籍信息合并到字典里
             cached_book_info = self._row_dict.select(book_id)
@@ -120,8 +117,7 @@ class Crawler(object):
 
         self._row_dict.commit()
 
-
-    def get_book_download(self, reflesh=False):
+    def crawl_book(self, refresh=False, interval=0.02):
 
         all_product = self._row_dict.select_all()
 
@@ -135,11 +131,9 @@ class Crawler(object):
             for book_id in books:
                 book = books[book_id]
 
-
                 columns = book['columns']
                 for column_id in columns:
                     column = columns[column_id]
-
 
                     column_title = column['title']
 
@@ -147,26 +141,27 @@ class Crawler(object):
 
                     all_columns[big_title] = column
 
-        pbar = tqdm(all_columns)
-        for title in pbar:
-            pbar.set_description('获取卷详情: {0}'.format(title))
+        column_process_bar = tqdm(all_columns)
+        for title in column_process_bar:
+            column_process_bar.set_description('获取卷详情: {0}'.format(title))
 
             column = all_columns[title]
 
-            if column.get('fetched', False) and (not reflesh):
+            if column.get('fetched', False) and (not refresh):
                 continue
 
             column_url = column['url']
             try:
-                column_img = Crawler.get_book_details(column_url, 0.05)
+                column_img = Crawler.crawl_book_column_pages(column_url, interval=interval)
                 column['img'] = column_img
                 column['fetched'] = True
                 self._row_dict.commit()
-            except:
+            except Exception as e:
+                traceback.print_exc(e)
                 column['fetched'] = False
                 self._row_dict.commit()
 
-    def download_book(self, reflesh=False, interval=0.02):
+    def download_book(self, refresh=False, interval=0.02):
 
         all_product = self._row_dict.select_all()
 
@@ -174,7 +169,6 @@ class Crawler(object):
 
         for pid in all_product:
             product = all_product[pid]
-            p_title = product['title']
             books = product['books']
 
             for book_id in books:
@@ -187,13 +181,14 @@ class Crawler(object):
                 for column_id in columns:
                     column = columns[column_id]
 
-                    column_dir = str(os.path.abspath(os.path.join(self._base_dir, book_version, column_fill_zero(column_id))))
+                    column_dir = str(
+                        os.path.abspath(os.path.join(self._base_dir, book_version, column_fill_zero(column_id))))
 
                     all_columns[column_dir] = column
 
-        pbar = tqdm(all_columns)
-        for column_dir in pbar:
-            pbar.set_description('下载卷: {0}'.format(column_dir))
+        column_process_bar = tqdm(all_columns)
+        for column_dir in column_process_bar:
+            column_process_bar.set_description('下载卷: {0}'.format(column_dir))
 
             column = all_columns[column_dir]
 
@@ -213,17 +208,18 @@ class Crawler(object):
             try:
                 page_fill_zero = build_fill_zero(len(column_img))
                 for i, img in enumerate(column_img):
-                    if img in column_img_d:
+                    if img in column_img_d and (not refresh):
                         continue
-
                     img_path = os.path.join(column_dir, page_fill_zero(str(i + 1)) + '.' + img.split('.')[-1])
                     download_img(img, img_path, True)
                     column_img_d.append(img)
                     self._row_dict.commit()
+
                     time.sleep(interval)
+
             except Exception as e:
-                print(e)
-                pass
+                traceback.print_exc(e)
+
 
 if __name__ == '__main__':
     # url = 'https://www.manhuadb.com/manhua/3481'
@@ -242,10 +238,7 @@ if __name__ == '__main__':
     #     json.dump(book_info, fp, indent=2)
     # get_book_list('伊藤润二')
 
-    base_dir = os.path.join('.', '_rst', 'manhuadb', '伊藤润二')
-    crawler = Crawler(base_dir)
-    # crawler.get_book_info_by_keyword('伊藤润二')
-    # crawler.get_book_download()
+    crawler = Crawler(os.path.join('.', '_rst', 'manhuadb', '伊藤润二'))
+    crawler.search_book('伊藤润二')
+    crawler.crawl_book(interval=0.01)
     crawler.download_book(interval=0.01)
-    # Crawler._get_book_info('3481')
-
